b58f7fe23063396a305f8d979dec62bfe811d217
Jose Duarte <joseduarte019@gmail.com>
2021-01-22 10:13:17 -0400

    fuck off

--- quasar.conf.js
+++ quasar.conf.js
@@ -42,7 +42,6 @@ module.exports = function (/* ctx */) {
 
     // Full list of options: https://quasar.dev/quasar-cli/quasar-conf-js#Property%3A-build
     build: {
-
       vueRouterMode: 'hash', // available values: 'hash', 'history'
 
       // transpile: false,

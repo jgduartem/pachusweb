9cdbf0bb91c12706d0ff42e8b9785c944682e27e
Jose Duarte <joseduarte019@gmail.com>
2021-01-22 10:08:16 -0400

    super f

--- quasar.conf.js
+++ quasar.conf.js
@@ -42,7 +42,6 @@ module.exports = function (/* ctx */) {
 
     // Full list of options: https://quasar.dev/quasar-cli/quasar-conf-js#Property%3A-build
     build: {
-      distDir: ctx.mode.spa ? 'public' : null,
 
       vueRouterMode: 'hash', // available values: 'hash', 'history'
 

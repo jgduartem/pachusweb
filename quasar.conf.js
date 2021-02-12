9906aa702b3503116ec22e6318d5b02aefef59c3
Jose Duarte <joseduarte019@gmail.com>
2021-02-12 15:22:46 -0400

    add pwa option

--- quasar.conf.js
+++ quasar.conf.js
@@ -118,7 +118,7 @@ module.exports = function (/* ctx */) {
         display: 'standalone',
         orientation: 'portrait',
         background_color: '#ffffff',
-        theme_color: '#027be3',
+        theme_color: '#F64965',
         icons: [
           {
             src: 'icons/icon-128x128.png',
